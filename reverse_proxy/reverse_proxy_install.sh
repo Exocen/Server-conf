f187f70ad62642c41784205fec61fd516969b2c7
Exocen <Exocen@users.noreply.github.com>
2020-05-11 16:27:08 -0600

    new nginx conf

---
+++ reverse_proxy/reverse_proxy_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install nginx
+# sudo rm /etc/nginx/site-enabled/default
+# ln -s /etc/nginx/site-enabled/default $HOME/default
+# cd $HOME && wget https://dl.eff.org/certbot-auto && chmod a+x certbot-auto
+# add to cron
+
+LOG_DIR="$HOME/reverse_proxy_log"
+if [ $# -eq 1 ]
+then
+    mkdir -p $LOG_DIR
+    {
+        cp default /tmp/default
+        IP=`curl ipinfo.io/ip`
+        sed -i 's/DESTINATION/'$IP'/g' /tmp/default
+        scp /tmp/default $1:"default"
+        ssh $1 ./certbot-auto --nginx -n --agree-tos --rsa-key-size 4096 --register-unsafely-without-email -d $1
+        ssh $1 sudo systemctl restart nginx
+    } 2>$LOG_DIR/reverse.err 1>$LOG_DIR/reverse.log
+fi
+
+# Local Variables:
+# mode: Shell-script
+# coding: mule-utf-8
+# End:
