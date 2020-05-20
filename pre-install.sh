bc98a1cfea8f75554084857f96ced31d6cc4dba6
Exocen <Exocen@users.noreply.github.com>
2020-05-20 16:41:43 -0600

    preinstall script updated

--- pre-install.sh
+++ pre-install.sh
@@ -7,7 +7,7 @@ read answer
 if [ "$answer" == "Y" ] || [ "$answer" == "y" ] || [ "$answer" == "YES" ] || [ "$answer" == "yes" ];then
     echo 'Pre-install.....'
     sudo hostnamectl set-hostname $hostname
-    sed -i 's/HOSTNAME/'$hostname'/g' website/default website/default_https website/install_web.sh mail/install.sh mail/main.cf mail/opendkim.conf
+    sed -i 's/HOSTNAME/'$hostname'/g' mail/install.sh mail/main.cf mail/opendkim.conf
 else
     echo 'Cancel'
 fi
