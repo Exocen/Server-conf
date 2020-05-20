0ea16e166e6adaccee4d3af597f007cd447341ce
Exocen <Exocen@users.noreply.github.com>
2020-05-20 15:56:25 -0600

    updated nginx conf

--- reverse_proxy/reverse_proxy_install.sh
+++ reverse_proxy/reverse_proxy_install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 LOCAL=`dirname "$(readlink -f "$0")"`
-# install nginx
 function install(){
+    # install nginx
     cd $HOME && wget https://dl.eff.org/certbot-auto
-    certbot_dir="/usr/local/bin/"
+    certbot_dir="/usr/bin/"
     certbot_name="certbot-auto"
-    sudo chown root $HOME/certbot-auto
-    sudo chmod 0755 $HOME/certbot-auto
-    sudo mv $HOME/certbot-auto $certbot_dir
-    ./$certbot_dir/certbot-auto
-    sudo rm /etc/nginx/sites-enabled/default
+    sudo chown root $HOME/$certbot_name
+    sudo chmod 0755 $HOME/$certbot_name
+    sudo mv $HOME/$certbot_name $certbot_dir
+    sudo ./$certbot_dir/$certbot_name --nginx -d $1
+    # get certifs
+    sudo rm /etc/nginx/conf.d/default.conf
     sudo ln -s $HOME/default /etc/nginx/conf.d/default.conf
 }
 
@@ -26,7 +27,7 @@ then
         IP=`curl ipinfo.io/ip`
         sed -i 's/DESTINATION/'$IP'/g' /tmp/default
         scp /tmp/default $1:"default"
-        ssh $1 ./certbot-auto renew --nginx -n --agree-tos --register-unsafely-without-email
+        ssh $1 sudo certbot-auto renew --nginx -n --agree-tos --register-unsafely-without-email
         ssh $1 sudo systemctl restart nginx
     } 2>$LOG_DIR/reverse.err 1>$LOG_DIR/reverse.log
 fi
